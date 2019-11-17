17c62d43d37acdd8285d7a19aa70731ce4b52461
Hugo Persson <hugopersson7@outlook.com>
2019-11-17 14:03:27 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let url;
+  import { Router, Route } from "svelte-routing";
+  import Login from "./Pages/Login.svelte";
+  import PageNotFound from "./Pages/404.svelte";
+</script>
+
+<style>
+
+</style>
+
+<Router {url}>
+  <div>
+    <Route path="/" component={Login} />
+    <Route path="*" component={PageNotFound} />
+  </div>
+</Router>
---
+++ src/Pages/Login.svelte
@@ -0,0 +1,21 @@
+<script>
+  function getOneTimeCode(e) {
+    e.preventDefault();
+    console.log("hey");
+  }
+</script>
+
+<style>
+  main {
+    text-align: center;
+  }
+</style>
+
+<main>
+  <h1>Login/Register</h1>
+  <form action="" on:click={login}>
+    <input type="text" name="email" id="enterEmail" placeholder="E-Mail" />
+    <input type="submit" value="Proceed" />
+
+  </form>
+</main>
