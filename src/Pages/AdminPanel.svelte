8230b2fd7335e00fab51df4f45213feca51a9768
Hugo Persson <hugopersson7@outlook.com>
2019-11-25 18:49:51 +0100

    Finished the look for all orders

--- src/Pages/AdminPanel.svelte
+++ src/Pages/AdminPanel.svelte
@@ -1,28 +1,33 @@
 <script>
   import { Link } from "svelte-routing";
+  import Order from "./Components/Order.svelte";
+
   let orders = [
     {
       name: "cool shit",
       number: 1,
-      from: "Hugo Persson"
+      customer: "Hugo Persson",
+      status: "Ej Påbörjad"
     },
     {
       name: "cool shit",
-      number: 1,
-      from: "Hugo Persson"
+      number: 2,
+      customer: "Hugo Persson",
+      status: "Påbörjad"
     },
     {
       name: "cool shit",
-      number: 1,
-      from: "Hugo Persson"
+      number: 3,
+      customer: "Hugo Persson",
+      status: "Avslutad"
     },
     {
       name: "cool shit",
-      number: 1,
-      from: "Hugo Persson"
+      number: 4,
+      customer: "Hugo Persson",
+      status: "Ej Påbörjad"
     }
   ];
-  import Order from "./Components/Order.svelte";
 </script>
 
 <nav class="navbar navbar-expand-lg sticky-top navbar-light bg-light">
@@ -37,23 +42,37 @@
     </div>
   </div>
 </nav>
-<div class="container-fluid">
+<div class="card m-sm-4 m-2 text-center">
+  <div class="card-header h3">Alla beställningar</div>
 
-  <div class="card m-4 text-center">
-    <div class="card-header h3">Orders</div>
-
-    <div class="container-fluid">
-      <div class="row text-center">
-        <div class="col-1">#</div>
-        <div class="col-4">Name</div>
-        <div class="col-3">Orderer</div>
-        <div class="col-2">Status</div>
-
-        <div class="col-2" />
-      </div>
+  <table class="table table-striped table-hover table-bordered ">
+    <caption>Klicka på en beställning för mer information</caption>
+    <thead class="thead-dark">
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Namn</th>
+        <th scope="col " class="d-none d-sm-table-cell">Beställare</th>
+        <th scope="col">Status</th>
+        <th scope="col">
+          <form
+            class="form-inline my-2 my-lg-0 text-center justify-content-end">
+            <input
+              class="form-control mr-sm-2 "
+              type="search"
+              placeholder="Sök"
+              aria-label="Sök" />
+            <button class="btn btn-success bg my-2 my-sm-0" type="submit">
+              Sök
+            </button>
+          </form>
+        </th>
+      </tr>
+    </thead>
+    <tbody>
       {#each orders as order}
         <Order {order} />
       {/each}
-    </div>
-  </div>
+
+    </tbody>
+  </table>
 </div>
--- src/Pages/Components/Order.svelte
+++ src/Pages/Components/Order.svelte
@@ -1,29 +1,27 @@
 <script>
   export let order;
+  let expanded = false;
+  function expand() {
+    expanded = true;
+  }
 </script>
 
 <style>
-  .row div {
-    border: 1px solid gray;
-    padding: 0.5rem;
+  tr {
     cursor: pointer;
-  }
-  .row div:hover {
-  }
-  .row:nth-child(odd) {
-  }
-  .row:nth-child(even) {
   }
 </style>
 
-<div class="row">
-  <div class="col-1">{order.number}</div>
-  <div class="col-4">{order.name}</div>
-  <div class="col-3">{order.from}</div>
-  <div class="col-2">{order.status}</div>
-
-  <div class="col-2">
-
-    <button type="button" class="btn btn-danger">Delte order</button>
-  </div>
-</div>
+{#if expanded}
+  expanded order
+{:else}
+  <tr on:click={expand}>
+    <th class="align-middle" scope="row">{order.number}</th>
+    <td class="align-middle">{order.name}</td>
+    <td class="align-middle d-none d-sm-table-cell">{order.customer}</td>
+    <td class="align-middle">{order.status}</td>
+    <td>
+      <button class="btn btn-danger">Delete order</button>
+    </td>
+  </tr>
+{/if}
