9292e52f1c794dd67fb3ac3a085e4bc31869379f
Hugo Persson <hugopersson7@outlook.com>
2019-11-25 15:42:46 +0100

    Draft adminPanel

--- src/Pages/AdminPanel.svelte
+++ src/Pages/AdminPanel.svelte
@@ -1,22 +1,59 @@
 <script>
-    import {
-        Link
-    } from "svelte-routing"
+  import { Link } from "svelte-routing";
+  let orders = [
+    {
+      name: "cool shit",
+      number: 1,
+      from: "Hugo Persson"
+    },
+    {
+      name: "cool shit",
+      number: 1,
+      from: "Hugo Persson"
+    },
+    {
+      name: "cool shit",
+      number: 1,
+      from: "Hugo Persson"
+    },
+    {
+      name: "cool shit",
+      number: 1,
+      from: "Hugo Persson"
+    }
+  ];
+  import Order from "./Components/Order.svelte";
 </script>
 
 <nav class="navbar navbar-expand-lg sticky-top navbar-light bg-light">
-    <span class="navbar-brand mb-0 h1">Admin panel</span>
+  <span class="navbar-brand mb-0 h1">Admin panel</span>
 
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-            <a class="nav-item nav-link active" href="/adminPanel/manageUser">Manage users</a>
-            <a class="nav-item nav-link" href="">Settings</a>
-        </div>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="/adminPanel/manageUser">
+        Manage users
+      </a>
+      <a class="nav-item nav-link" href="">Settings</a>
     </div>
+  </div>
 </nav>
 <div class="container-fluid">
 
-    <div class="card m-4">
-        <div class="card-header">Orders</div>
+  <div class="card m-4 text-center">
+    <div class="card-header h3">Orders</div>
+
+    <div class="container-fluid">
+      <div class="row text-center">
+        <div class="col-1">#</div>
+        <div class="col-4">Name</div>
+        <div class="col-3">Orderer</div>
+        <div class="col-2">Status</div>
+
+        <div class="col-2" />
+      </div>
+      {#each orders as order}
+        <Order {order} />
+      {/each}
     </div>
+  </div>
 </div>
---
+++ src/Pages/Components/Order.svelte
@@ -0,0 +1,29 @@
+<script>
+  export let order;
+</script>
+
+<style>
+  .row div {
+    border: 1px solid gray;
+    padding: 0.5rem;
+    cursor: pointer;
+  }
+  .row div:hover {
+  }
+  .row:nth-child(odd) {
+  }
+  .row:nth-child(even) {
+  }
+</style>
+
+<div class="row">
+  <div class="col-1">{order.number}</div>
+  <div class="col-4">{order.name}</div>
+  <div class="col-3">{order.from}</div>
+  <div class="col-2">{order.status}</div>
+
+  <div class="col-2">
+
+    <button type="button" class="btn btn-danger">Delte order</button>
+  </div>
+</div>
