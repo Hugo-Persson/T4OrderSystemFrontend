b5da3506eb78c06c4f3cb8c47237e7cc8fbb4b56
Hugo Persson <hugopersson7@outlook.com>
2020-02-19 08:54:03 +0100

    Made it possible for clients to see the responsible admins email

--- src/Pages/Components/CustomerViewOrder.svelte
+++ src/Pages/Components/CustomerViewOrder.svelte
@@ -6,7 +6,15 @@
   <th scope="col">{order.productNumber}</th>
   <td>{order.productName}</td>
   <td>{order.customer.name}</td>
-  <td>{order.responsible.name}</td>
+  <td>
+    {order.responsible.name}
+    <br />
+    <a
+      href="mailto:{order.responsible.email === 'Nobody' ? '' : order.responsible.email}">
+      {order.responsible.email === 'Nobody' ? '' : order.responsible.email}
+    </a>
+
+  </td>
   <td>{order.status}</td>
   <td>{new Date(order.date).toDateString()}</td>
   <td>
